d604c501e23f720a095de73e7f04997ae409fa9c
ranyery <ranyerysc@gmail.com>
2022-12-17 20:53:58 -0300

    chore: added uuid set name file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import multer from "multer";
 import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 // import { StatusCodes as HTTP_STATUS_CODES } from "http-status-codes";
 
@@ -11,12 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let filename = "";
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, "./uploads/");
   },
   filename(req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+    filename = uuidv4() + path.extname(file.originalname);
+    cb(null, filename);
   },
 });
 
@@ -24,6 +28,7 @@ const upload = multer({ storage });
 
 app.post("/", upload.single("image"), async (request, response) => {
   const { file } = request;
+  const filePath = `./../uploads/${filename}`;
 
   response.send({ message: "Ok!" });
 });
