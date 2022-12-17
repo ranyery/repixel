2d7474caf0d91bfe857ec0dd101304c9b60bfe33
ranyery <ranyerysc@gmail.com>
2022-12-17 20:31:19 -0300

    feat(backend): add multer to handle image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import cors from "cors";
 import express from "express";
+import multer from "multer";
+import path from "path";
+
 // import { StatusCodes as HTTP_STATUS_CODES } from "http-status-codes";
 
 const PORT = process.env.PORT || 3333;
@@ -8,8 +11,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/", (request, response) => {
-  response.send({ message: "Hello, World!" });
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/", upload.single("image"), async (request, response) => {
+  const { file } = request;
+
+  response.send({ message: "Ok!" });
 });
 
 app.listen(PORT, () => {
