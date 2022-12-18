7147cd2873bddf74df43b8af374458c5e569fa65
ranyery <ranyerysc@gmail.com>
2022-12-18 00:05:11 -0300

    feat: add sharp, the best lib to resize and so on

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
+// https://www.freecodecamp.org/news/how-to-make-image-upload-easy-with-angular-1ed14cb2773b/
+// https://github.com/expressjs/multer/blob/master/doc/README-pt-br.md
+// https://www.npmjs.com/package/sharp
 import cors from "cors";
 import express from "express";
+import { fileTypeFromBuffer } from 'file-type';
+// import imagemin from "imagemin";
+// import imageminMozjpeg from "imagemin-mozjpeg";
 import multer from "multer";
-import path from "path";
-import { v4 as uuidv4 } from "uuid";
+import sharp from "sharp";
 
 // import { StatusCodes as HTTP_STATUS_CODES } from "http-status-codes";
 
@@ -12,25 +17,45 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let filename = "";
+// Armazena a imagem na pastas uploads
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "./uploads/");
+//   },
+//   filename(req, file, cb) {
+//     filename = uuidv4() + path.extname(file.originalname);
+//     cb(null, filename);
+//   },
+// });
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename(req, file, cb) {
-    filename = uuidv4() + path.extname(file.originalname);
-    cb(null, filename);
-  },
-});
+// const upload = multer({ storage });
 
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 app.post("/", upload.single("image"), async (request, response) => {
-  const { file } = request;
-  const filePath = `./../uploads/${filename}`;
+  const { originalname, buffer, size } = request.file;
 
-  response.send({ message: "Ok!" });
+  // const jpegBuffer = await imagemin.buffer(buffer, {
+  //   plugins: [imageminMozjpeg({ quality: 50 })],
+  // });
+
+  // const { ext, mime } = await fileTypeFromBuffer(jpegBuffer);
+
+  // const jpegBase64 = jpegBuffer.toString("base64");
+
+  const pngBuffer = await sharp(buffer).resize(300, 300, {fit: "contain"}).png({}).toBuffer();
+  const { ext, mime } = await fileTypeFromBuffer(pngBuffer);
+  const pngBase64 = pngBuffer.toString("base64");
+
+  response.send({
+    originalname,
+    oldSize: size,
+    newSize: 0,
+    diffSize: 0,
+    ext,
+    base64: `data:${mime};base64,${pngBase64}`,
+  });
 });
 
 app.listen(PORT, () => {
