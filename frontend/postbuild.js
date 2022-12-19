1a91a8513ed19ce3cd49e7c1975c1503f769bfe5
ranyery <ranyerysc@gmail.com>
2022-12-19 19:28:25 -0300

    feat: add postbuild script

---
+++ frontend/postbuild.js
@@ -0,0 +1,46 @@
+// https://dev.to/dylanvdmerwe/reduce-angular-style-size-using-purgecss-to-remove-unused-styles-3b2k
+const { exec } = require("child_process");
+const { statSync, readdirSync } = require("fs");
+const { extname } = require("path");
+
+const files = getFilesFromPath("./dist", ".css");
+const data = [];
+
+if (!files && files.length <= 0) {
+  console.log("cannot find style files to purge");
+  return;
+}
+
+for (const f of files) {
+  const originalSize = getFilesizeInKiloBytes("./dist/" + f) + "kb";
+  const o = { file: f, originalSize: originalSize, newSize: "" };
+  data.push(o);
+}
+
+console.log("Running PurgeCSS...");
+
+exec(
+  "purgecss -css dist/*.css --content dist/index.html dist/*.js -o dist/",
+  function (error, stdout, stderr) {
+    console.log("PurgeCSS done");
+    console.log();
+
+    for (const d of data) {
+      const newSize = getFilesizeInKiloBytes("./dist/" + d.file) + "kb";
+      d.newSize = newSize;
+    }
+
+    console.table(data);
+  }
+);
+
+function getFilesizeInKiloBytes(filename) {
+  const stats = statSync(filename);
+  const fileSizeInBytes = stats.size / 1024;
+  return fileSizeInBytes.toFixed(2);
+}
+
+function getFilesFromPath(dir, extension) {
+  const files = readdirSync(dir);
+  return files.filter((e) => extname(e).toLowerCase() === extension);
+}
