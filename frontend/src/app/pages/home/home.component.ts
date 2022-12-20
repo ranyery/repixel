f59edb3e7ee9da7c451efc000d0ee8091c43ff48
ranyery <ranyerysc@gmail.com>
2022-12-20 13:45:58 -0300

    chore(home): change home page

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ImageService } from 'src/app/shared/services/image.service';
-
-import { ImageSnippet } from '../../shared/services/image.service';
 
 @Component({
   selector: 'app-home',
@@ -9,43 +6,7 @@ import { ImageSnippet } from '../../shared/services/image.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public uploadedFiles: File[] = [];
-  private selectedFile!: any;
-
-  public hasImageConverted: boolean = false;
-  public convertedImage: any;
-
-  constructor(private imageService: ImageService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  uploadHandler({ files }: { files: File[] }) {
-    if (!files) return;
-    this.hasImageConverted = false;
-
-    for (let f of files) {
-      this.uploadedFiles.push(f);
-    }
-
-    const file = files[0];
-    const reader = new FileReader();
-
-    reader.addEventListener('load', (event: any) => {
-      console.log('Event image:', event);
-
-      this.selectedFile = new ImageSnippet(event.target.result, file);
-
-      this.imageService.upload(this.selectedFile.file).subscribe({
-        next: (data) => {
-          this.convertedImage = data;
-          this.hasImageConverted = true;
-        },
-        error: (error) => {
-          console.log('🔴 Error:', error);
-        },
-      });
-    });
-
-    reader.readAsDataURL(file);
-  }
 }
