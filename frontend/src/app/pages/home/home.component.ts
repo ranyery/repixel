97c75804675af139075e6bb5a95a0fc7cbefa85d
ranyery <ranyerysc@gmail.com>
2023-01-04 13:56:34 -0300

    feat(home): added tools items

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IItemProps } from './models/ItemProps';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public items: IItemProps[] = [
+    {
+      icon: 'compress',
+      title: 'Compress IMAGE',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa optio libero, itaque sunt impedit tenetur accusamus alias animi non quis.',
+      link: 'comprimir-imagem',
+    },
+    {
+      icon: 'resize',
+      title: 'Resize IMAGE',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa optio libero, itaque sunt impedit tenetur accusamus alias animi non quis.',
+      link: 'redimensionar-imagem',
+    },
+    {
+      icon: 'crop',
+      title: 'Crop IMAGE',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa optio libero, itaque sunt impedit tenetur accusamus alias animi non quis.',
+      link: 'cortar-imagem',
+    },
+    {
+      icon: 'convert',
+      title: 'Convert to JPG',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa optio libero, itaque sunt impedit tenetur accusamus alias animi non quis.',
+      link: 'converter-imagem',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
